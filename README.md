642da27403ef130f4f9a2594d42cfa1b4f1682c7
satanch <itdevroman@yandex.ru>
2019-10-28 23:45:25 +0300

    0.0.1
    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # unipresser
-Node.js native mouse move, mouse click and keyboard typing
+
+** WARNING! Development in progress **
+
+This library allows you to get mouse position, move mouse and perform mouse clicks from your Node.js script.
+
+* Python is not required!
+* Java is not required!
+* External software is not required!
+
+Everything is working in native way, with C++ Node.js native addons.
+
+## Current status
+
+This library in development status, so don't use it for production purposes.
+
+### OS implementations
+
+| OS | Status |
+| --- | --- |
+| win32 | [Implemented](https://github.com/satanch/unipresser-win32) |
+| darwin | Not implemented |
+| linux | Not implemented |
+
+## Installation
+
+This library will automatically download required native addon for your specs. Simply `require` it and use.
+
+## Available methods
+
+Every method is sync, without callback or promise.
+
+### getMouseCursor
+
+Returns an array with current mouse coordinates.
+
+```js
+getMouseCursor();
+```
+
+### setMouseCursor
+
+Moves mouse cursor to specified position.
+
+```js
+setMouseCursor(x, y);
+```
+
+### emitMouseClick
+
+Clicks with a specified button at the current mouse position.
+
+```js
+emitMouseClick('left');
+emitMouseClick('right');
+```
---
+++ install.js
@@ -0,0 +1,57 @@
+/*
+    Unipresser by satanch
+    Native addon download script
+*/
+
+const axios = require('axios');
+const fs = require('fs');
+
+(async () => {
+    try {
+        console.log('✨  Looking for a native addon for your os and node version...');
+
+        const addonRepos = {
+            win32: 'satanch/unipresser-win32'
+        };
+
+        if (typeof addonRepos[process.platform] !== 'string')
+            throw new Error(`Addon for your operating system (${process.platform}) is not implemented yet!`);
+
+        const releaseRequest = await axios({
+            method: 'GET',
+            url: `https://api.github.com/repos/${addonRepos[process.platform]}/releases/latest`,
+            headers: {
+                Accept: 'application/vnd.github.v3+json'
+            }
+        });
+
+        let addonUrl = false;
+
+        for (let asset of releaseRequest.data.assets) {
+            if (asset.name.indexOf(process.version) !== -1)
+                addonUrl = asset.browser_download_url;
+        }
+
+        if (!addonUrl)
+            throw new Error(`There is no compiled addon for Node.js version: ${process.version}`);
+
+        console.log(`⬇️  Downloading native addon...`);
+
+        const addonRequest = await axios({
+            method: 'GET',
+            url: addonUrl,
+            responseType: 'arraybuffer'
+        });
+
+
+        console.log(`💾  Saving native addon...`);
+
+        fs.mkdirSync('./.addon');
+        fs.writeFileSync('./.addon/unipresser.node', addonRequest.data, {encoding: null});
+
+        console.log(`✔️  Done!`);
+    } catch (err) {
+        console.error('Native addon is not downloaded due error!');
+        console.error(err);
+    }
+})();
